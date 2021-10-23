cc051fb08282a6bb4a738f9adef4dba92f6c98ee
Wiktor Kamiński <wkaminski@qualtrics.com>
2021-10-23 23:16:46 +0200

    WIP: set up redux, create form for simulations

---
+++ src/features/map/mapApi.ts
@@ -0,0 +1,32 @@
+import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
+
+import { CreateMapRequest } from './requests';
+import { SimulationMap } from './types';
+
+export const mapApi = createApi({
+  reducerPath: 'mapApi',
+  baseQuery: fetchBaseQuery({ baseUrl: process.env.API_URL || 'http://localhost:8080/' }),
+  tagTypes: ['Map', 'Id'],
+  endpoints: (builder) => ({
+    getMapById: builder.query<SimulationMap, number>({
+      query: (id) => ({ url: `map/${id}` }),
+      providesTags: (result) => 
+        (result ? [{ type: 'Map', id: result.id }, 'Map'] : ['Map']),
+    }),
+    getAllMapIds: builder.query<number[], void>({
+      query: () => ({ url: 'simulation/all' }),
+      providesTags: ['Id'],
+    }),
+    createMap: builder.mutation<SimulationMap, CreateMapRequest>({
+      query: (request) => ({ url: 'simulation/create', method: 'POST', body: request }),
+      invalidatesTags: (result) => 
+        (result ? [{ type: 'Map', id: result.id }, 'Map'] : ['Map']),
+    }),
+  }),
+});
+
+export const {
+  useCreateMapMutation,
+  useGetMapByIdQuery,
+  useGetAllMapIdsQuery,
+} = mapApi;
---
+++ src/features/simulation/simulationApi.ts
@@ -0,0 +1,47 @@
+import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
+
+import type {
+  Simulation, SimplifiedSimulation,
+} from './types';
+import type {
+  CreateSimulationRequest, SimulateRequest,
+} from './requests';
+
+export const simulationApi = createApi({
+  reducerPath: 'simulationApi',
+  baseQuery: fetchBaseQuery({ baseUrl: process.env.API_URL || 'http://localhost:8080/' }),
+  tagTypes: ['Simulation', 'SimplifiedSimulation'],
+  endpoints: (builder) => ({
+    getSimulationById: builder.query<Simulation, number>({
+      query: (id) => ({ url: `simulation/${id}` }),
+      providesTags: (result) =>  
+        (result ? [{ id: result.id, type: 'Simulation' as const }, 'Simulation'] : ['Simulation']),
+    }),
+    getAllSimulations: builder.query<SimplifiedSimulation[], void>({
+      query: () => ({ url: 'simulation/all' }),
+      providesTags: ['SimplifiedSimulation'],
+    }),
+    createSimulation: builder.mutation<Simulation, CreateSimulationRequest>({
+      query: (request) => ({ url: 'simulation/create', method: 'POST', body: request }),
+      invalidatesTags: ['Simulation', 'SimplifiedSimulation'],
+    }),
+    simulate: builder.mutation<Simulation, SimulateRequest>({
+      query: ({ id, times }) => ({ url: `simulation/simulate?id=${id}&times=${times}`, method: 'POST' }),
+      invalidatesTags: (result) => 
+        (result ? [{ id: result.id, type: 'Simulation' as const }, 'Simulation'] : ['Simulation']),
+    }),
+    deleteSimulation: builder.mutation<void, number>({
+      query: (id) => ({ url: `simulation/delete?id=${id}`, method: 'DELETE' }),
+      invalidatesTags: ['Simulation', 'SimplifiedSimulation'],
+    }),
+  }),
+});
+
+
+export const {
+  useCreateSimulationMutation,
+  useGetAllSimulationsQuery,
+  useGetSimulationByIdQuery,
+  useSimulateMutation,
+  useDeleteSimulationMutation,
+} = simulationApi;
