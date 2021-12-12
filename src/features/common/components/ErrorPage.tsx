989c430f57b0d00d1a8932120e51dc43ff9ff206
Wiktor Kamiński <kamykikamienie@gmail.com>
2021-12-12 15:39:19 +0100

    added files

---
+++ src/features/common/components/ErrorPage.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+export default function ErrorPage(): JSX.Element{
+  return (
+        <div>
+            Something went wrong when loading data. Try refreshing the page. 
+        </div>
+  );
+}
