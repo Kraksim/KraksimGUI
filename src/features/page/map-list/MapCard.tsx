76f00c619f04279609ba61d416bfd1537dbd1a77
Grzegorz Poreba <gporeba@atlassian.com>
2021-12-15 04:26:35 +0100

    Added graph to stats, small tweaks

--- src/features/page/map-list/MapCard.tsx
+++ src/features/page/map-list/MapCard.tsx
@@ -39,32 +39,29 @@ export default function MapCard({ map, loading } : Props): JSX.Element{
           {loading && <Skeleton variant="text" width="60%"/>}
         </Typography>
         <Box sx={{ display: 'flex', flexWrap: 'wrap' }}>
-          {map && map.compatibleWith
-            .map((movementStrategy, index) => <Chip 
-            sx={{ margin: '4px 4px 4px 0px' }} 
-            size="small" 
-            key={index} 
-            label={labelMovementStrategy(movementStrategy)}/>)}
+          {map && [...map.compatibleWith
+            .map((movementStrategy, index) => <Chip
+            sx={{ margin: '4px 4px 4px 0px' }}
+            size="small"
+            key={index}
+            label={labelMovementStrategy(movementStrategy)}/>),
+              <Chip
+                  sx={{ margin: '4px 4px 4px 0px' }}
+                  size="small"
+                  key={map.id}
+                  style={{ backgroundColor:'rgba(135, 193, 207, 0.31)' }}
+                  label={`Simulations: ${map.simulationsCount}`}/>,
+          ]}
           {loading && <>
+          <Skeleton sx={{ margin: '4px 4px 4px 0px' }} variant="rectangular" width="70%"/>
           <Skeleton sx={{ margin: '4px 4px 4px 0px' }} variant="rectangular" width="70%"/>
           <Skeleton sx={{ margin: '4px 4px 4px 0px' }} variant="rectangular" width="70%"/>
           </>}
         </Box>
-          <Box sx={{ display: 'flex', flexWrap: 'wrap' }}>
-              {map && <Chip
-                      sx={{ margin: '4px 4px 4px 0px' }}
-                      size="small"
-                      key={map.id}
-                      style={{ backgroundColor:'rgba(135, 193, 207, 0.31)' }}
-                      label={`Simulations: ${map.simulationsCount}`}/>}
-              {loading && <>
-                  <Skeleton sx={{ margin: '4px 4px 4px 0px' }} variant="rectangular" width="70%"/>
-              </>}
-          </Box>
         {map && <Typography variant="body2" color="text.secondary">
           {map.description}
         </Typography>}
-        {loading && 
+        {loading &&
         <>
         <Skeleton/>
         <Skeleton/>
