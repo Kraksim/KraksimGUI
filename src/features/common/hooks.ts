d0fdef4a1765d9c716ceb1558e786af2c5a9432c
Grzegorz Poreba <gporeba@atlassian.com>
2021-12-29 16:28:41 +0100

    Made drawer responsive on phones

--- src/features/common/hooks.ts
+++ src/features/common/hooks.ts
@@ -1,5 +1,29 @@
 import { useLocation } from 'react-router-dom';
+import { useState, useEffect } from 'react';
 
 export function useUrlParamsQuery(): URLSearchParams {
   return new URLSearchParams(useLocation().search);
+}
+
+function getWindowDimensions() {
+  const { innerWidth: width, innerHeight: height } = window;
+  return {
+    width,
+    height,
+  };
+}
+
+export default function useWindowDimensions(): { width: number; height: number } {
+  const [windowDimensions, setWindowDimensions] = useState(getWindowDimensions());
+
+  useEffect(() => {
+    function handleResize() {
+      setWindowDimensions(getWindowDimensions());
+    }
+
+    window.addEventListener('resize', handleResize);
+    return () => window.removeEventListener('resize', handleResize);
+  }, []);
+
+  return windowDimensions;
 }
--- src/features/page/menu/PageMenu.tsx
+++ src/features/page/menu/PageMenu.tsx
@@ -1,5 +1,5 @@
 import {
-  Drawer, List, ListItem, ListItemIcon, ListItemText, Divider, Box, IconButton, 
+  Drawer, List, ListItem, ListItemIcon, ListItemText, Divider, Box, IconButton,
 } from '@mui/material';
 import React, { useEffect, useState } from 'react';
 import MapIcon from '@mui/icons-material/Map';
@@ -8,23 +8,25 @@ import QueryStatsIcon from '@mui/icons-material/QueryStats';
 import CreateIcon from '@mui/icons-material/Create';
 import { useHistory } from 'react-router-dom';
 import styled from '@emotion/styled';
-import { isMobile } from 'react-device-detect';
+import SvgIcon from '@mui/material/SvgIcon/SvgIcon';
 
 import CompareSimulationDialog from '../home/dialogs/CompareSimulationsDialog';
+import useWindowDimensions from '../../common/hooks';
 
 const EXPANDED_WIDTH = '250px';
 const COLLAPSED_WIDTH = '66px';
+const MAX_XD = 1006;
 
 const ItemText = styled(ListItemText)(() => ({
   whiteSpace: 'nowrap',
 }));
 
-export default function PageMenu(): JSX.Element{ // todo make it look good on phones
+export default function PageMenu(): JSX.Element {
   const [compareSimulationsDialogOpened, setCompareSimulationsDialogOpened] = useState(false);
+  const { width:windowWidth } = useWindowDimensions();
 
   const history = useHistory();
   const [currentPath, setCurrentPath] = useState(history.location.pathname);
-
 
   useEffect(() => {
     return history.listen((location) => {
@@ -48,69 +50,115 @@ export default function PageMenu(): JSX.Element{ // todo make it look good on ph
     history.push('/');
   };
 
-  const normalTabStyle = { marginBottom:'16px', color:'#A3AED0', borderRadius: '5px' };
-  const currentTabStyle = { ...normalTabStyle,
+  const isBigScreen = windowWidth >= MAX_XD;
+  const drawerWidth = isBigScreen ? EXPANDED_WIDTH : COLLAPSED_WIDTH;
+  const listPadding = {
+    padding: '30px',
+
+    '@media (max-width: 1006px)': {
+      padding: '15px 5px 0 5px',
+    },
+
+  };
+  const logoSx = {
+    padding: '18px 0px 22px 30px',
+    '@media (max-width: 1006px)': {
+      padding: '0',
+    },
+  };
+  return (
+    <>
+        <Drawer variant="permanent" sx={{ width: drawerWidth }}>
+            <Box width={drawerWidth}>
+                <Box sx={logoSx}>
+                    <IconButton onClick={onLogoClick}>
+                        <img alt="kraksim-logo"
+                             style={{ width: '50px', height: '50px' }}
+                             src={'/logo192.png'}
+                        />
+                    </IconButton>
+                </Box>
+                <Divider/>
+                <List sx={listPadding}>
+                    <PageListItem
+                        text="Maps"
+                        clicked={currentPath === '/maps/all'}
+                        onClick={onViewMapsClicked}
+                        Icon={MapIcon}
+                        expanded={isBigScreen}/>
+                    <PageListItem
+                        text="Simulations"
+                        clicked={currentPath === '/simulations/all'}
+                        onClick={onViewSimulationsClicked}
+                        Icon={UpdateIcon}
+                        expanded={isBigScreen}/>
+                    <PageListItem
+                        text="Comparator"
+                        clicked={currentPath === '/simulations/compare'}
+                        onClick={() => setCompareSimulationsDialogOpened(true)}
+                        Icon={QueryStatsIcon}
+                        expanded={isBigScreen}/>
+                    <PageListItem
+                        text="Map creator"
+                        clicked={currentPath === '/maps/create'}
+                        onClick={onCreateMapClicked}
+                        Icon={CreateIcon}
+                        expanded={isBigScreen}/>
+                </List>
+            </Box>
+        </Drawer>
+        <CompareSimulationDialog
+            afterConfirm={() => setCompareSimulationsDialogOpened(false)}
+            open={compareSimulationsDialogOpened}
+            onClose={() => setCompareSimulationsDialogOpened(false)}
+        />
+    </>
+  );
+}
+
+interface Props {
+  text: string;
+  clicked: boolean;
+  onClick: () => void;
+  Icon: typeof SvgIcon;
+  expanded: boolean
+}
+
+export function PageListItem({
+  text, clicked, onClick, Icon, expanded,
+}: Props) : JSX.Element{
+
+  const normalTabStyle = {
+    marginBottom: '16px',
+    color: '#A3AED0',
+    borderRadius: '5px',
+    '@media (max-width: 1006px)': {
+      paddingLeft: '16px',
+    },
+  };
+  const currentTabStyle = {
+    ...normalTabStyle,
     backgroundColor: '#337180',
     color: '#FFFFFF',
     ':hover': {
       bgcolor: '#337180',
-    } };
+    },
+  };
 
-  return (
-    <>
-    <Drawer variant="permanent" sx={{ width: isMobile ? COLLAPSED_WIDTH : EXPANDED_WIDTH }}>
-        <Box width={ isMobile ? COLLAPSED_WIDTH : EXPANDED_WIDTH }>
-            <Box sx={{ padding: '18px 0px 22px 30px' }}>
-            <IconButton onClick={onLogoClick}>
-                <img alt="kraksim-logo" 
-                style={{ width: '50px', height: '50px' }} 
-                src={'/logo192.png'} 
-                />
-            </IconButton>
-            </Box>
-            <Divider />
-            <List  sx={{ padding: '30px' }}>
-                <ListItem sx = { currentPath === '/maps/all' ? currentTabStyle : normalTabStyle }
-                          button
-                          onClick={onViewMapsClicked}>
-                    <ListItemIcon>
-                        <MapIcon sx ={{ color: currentPath === '/maps/all' ? '#FFFFFF' : '#A3AED0' }}/>
-                    </ListItemIcon>
-                    <ItemText primary="Maps"/>
-                </ListItem>
-                <ListItem sx = { currentPath === '/simulations/all' ? currentTabStyle : normalTabStyle }
-                    button
-                    onClick={onViewSimulationsClicked}>
-                    <ListItemIcon>
-                        <UpdateIcon sx = {{ color: currentPath ===  '/simulations/all' ? '#FFFFFF' : '#A3AED0' }}/>
-                    </ListItemIcon>
-                    <ItemText primary="Simulations"/>
-                </ListItem>
-                <ListItem sx = { currentPath.includes('/simulations/compare') ? currentTabStyle : normalTabStyle }
-                    button
-                    onClick={() => setCompareSimulationsDialogOpened(true)}>
-                    <ListItemIcon>
-                        <QueryStatsIcon
-                            sx = {{ color: currentPath ===  '/simulations/compare' ? '#FFFFFF' : '#A3AED0' }}/>
-                    </ListItemIcon>
-                    <ItemText sx={{ whiteSpace: 'nowrap' }} primary="Comparator"/>
-                </ListItem>
-                <ListItem sx = { currentPath === '/maps/create' ? currentTabStyle : normalTabStyle }
-                    button
-                    onClick={onCreateMapClicked}>
-                    <ListItemIcon>
-                        <CreateIcon sx ={{ color: currentPath === '/maps/create' ? '#FFFFFF' : '#A3AED0' }}/>
-                    </ListItemIcon>
-                    <ItemText primary="Map creator"/>
-                </ListItem>
-            </List>
-        </Box>
-    </Drawer>
-    <CompareSimulationDialog
-        afterConfirm={() => setCompareSimulationsDialogOpened(false)}
-        open={compareSimulationsDialogOpened}
-        onClose={() => setCompareSimulationsDialogOpened(false)}
-    />
-    </>
-  );
+
+  const styledIcon = <Icon sx={{ color: clicked ? '#FFFFFF' : '#A3AED0' }}/>;
+  return <ListItem
+        sx={clicked ? currentTabStyle : normalTabStyle}
+        button
+        onClick={onClick}>
+        {
+            expanded
+              ? <ListItemIcon>
+                    {styledIcon}
+                </ListItemIcon>
+              : styledIcon
+        }
+
+        {expanded ? <ItemText primary={text}/> : null}
+    </ListItem>;
 }
